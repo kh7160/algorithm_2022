23f34bfb4e4e5beedf81dd216d81fe38ac59ca5e
kh7160 <kh7160@naver.com>
2022-07-05 21:06:05 +0900

    7/5

--- BOJ_1193.py
+++ BOJ_1193.py
@@ -1,2 +1,28 @@
 num = int(input())
 
+direction = True
+add = 1
+tot = 0
+while True:
+
+    tot += add
+    if tot >= num:
+        # print(f'tot : {tot}, add : {add}, direction : {direction}')
+        break
+
+    add += 1
+    direction = not direction
+
+start = tot - add + 1
+if direction:
+    a, b = add, 1
+else:
+    a, b = 1, add
+
+for i in range(start, num):
+    if direction:
+        a, b = a - 1, b + 1
+    else:
+        a, b = a + 1, b - 1
+
+print(f'{a}/{b}')
