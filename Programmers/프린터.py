9433dd13e0c7f94ce4a17c589dd574c656ba1c8e
kh7160 <kh7160@naver.com>
2022-09-01 20:35:25 +0900

    9/1

--- Programmers/프린터.py
+++ Programmers/프린터.py
@@ -1,34 +1,29 @@
 from collections import deque
 
-
 def solution(priorities, location):
     answer = 0
-    queue = deque(priorities)
-    max_ = max(queue)
+    priorities = deque(priorities)
     while True:
-        left_ = queue.popleft()
-        # print(f'queue, location : {queue, location}')
-        if max_ == left_:
+        max_ = max(priorities)
+        first = priorities.popleft()
+        # print(f'max_, first : {max_, first}')
+        if first != max_:
+            priorities.append(first)
+            location = location - 1 if location != 0 else len(priorities)-1
+        else:
             answer += 1
             if location == 0:
                 break
             else:
-                # print(f'max_queue, location : {queue, location}')
-                max_ = max(queue)
-                location = location - 1 if location > 0 else len(queue)-1
-                continue
-
-        queue.append(left_)
-        location = location - 1 if location > 0 else len(queue)-1
-        # print(f'queue, location : {queue, location}')
-
+                location -= 1
+        # print(f'location : {location}')
+        
     # print(f'answer : {answer}')
     return answer
 
-
 if __name__ == '__main__':
-    priorities = [2,1,3,2]
-    location = 2
+    priorities = [2, 1, 3, 2]
     # priorities = [1, 1, 9, 1, 1, 1]
+    location = 2
     # location = 0
-    solution(priorities=priorities, location=location)
+    solution(priorities, location)
